404528955cd235b398ee375e61d27dbd77b4b6d3
Ayenelu <147351508+Ayenelu@users.noreply.github.com>
2023-11-07 21:23:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectado ao banco de dados");
+// mais um console log de teste:
+console.log("O paulo fez commit sem voce ver na aula!");
+console.log("Enviado para GitHup");
